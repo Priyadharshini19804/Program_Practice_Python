6748ae6b9328d10e36fc7f0a54219de513b6985b
Priyadharshini S M <167222422+Priyadharshini19804@users.noreply.github.com>
2025-06-19 10:21:46 +0530

    Smallest_Element.py

---
+++ Smallest_Element.py
@@ -0,0 +1,13 @@
+'''
+
+ Write a code to Print the smallest element of the array .
+'''
+
+arr = [10, 89, 9, 56, 4, 80, 8]
+mini = arr[0]
+
+for i in range(len(arr)):
+  if arr[i] < mini:
+     mini = arr[i]
+
+print (mini)
